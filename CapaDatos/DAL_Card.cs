03f97edef40756eafa3c07473c71e82c3c736fda
Jeim05 <lopezvegajeimmy@gmail.com>
2024-09-12 22:23:52 -0600

    Creación de las capas y la conexion con la base de datos

---
+++ CapaDatos/DAL_Card.cs
@@ -0,0 +1,52 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CapaDatos
+{
+    public class DAL_Card
+    {
+        public List<Card> ObtenerCards()
+        {
+            List<Card> lista = new List<Card>();
+
+            try
+            {
+                using (SqlConnection oConexion = new SqlConnection(Conexion.conexion))
+                {
+                    string query = " SELECT IdCard, TituloPrincipal, SubTitulo, TextoCardInformativo FROM CARD";
+                    SqlCommand cmd = new SqlCommand(query, oConexion);
+                    cmd.CommandType = CommandType.Text;
+                    oConexion.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read()) {
+                            lista.Add(
+                                new Card()
+                                {
+                                    IdCard = Convert.ToInt32(reader["IdCard"]),
+                                    TituloPrincipal = reader["TituloPrincipal"].ToString(),
+                                    SubTitulo = reader["Subtitulo"].ToString(),
+                                    TextoCardInformativo = reader["TextoCardInformativo"].ToString(),
+                                });
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                lista = new List<Card>();
+                
+            }
+            return lista;
+        }
+
+    }
+}
---
+++ CapaEntidad/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Card
+    {
+        public int IdCard { get; set; }
+        public string TituloPrincipal { get; set; }
+        public string SubTitulo {  get; set; }
+        public string TextoCardInformativo { get; set; }
+    }
+}
