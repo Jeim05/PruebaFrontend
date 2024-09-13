96b2dbe20f6631b75f326514fc6734731188f57e
Jeim05 <lopezvegajeimmy@gmail.com>
2024-09-12 22:35:26 -0600

    Incorporación de las referencias entre capas

---
+++ CapaNegocio/BLL_Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class BLL_Card
+    {
+        private BLL_Card objCapaDatos = new BLL_Card();
+
+        public List<Card> ObtenerCards()
+        {
+            return objCapaDatos.ObtenerCards();
+        }
+    }
+}
