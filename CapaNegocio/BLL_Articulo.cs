7c71374298f51ed61f7914bb0a7c09e749a89a7b
Jeim05 <lopezvegajeimmy@gmail.com>
2024-09-15 21:23:06 -0600

    Craecion de la clase de negocio de articulo

---
+++ CapaNegocio/BLL_Articulo.cs
@@ -0,0 +1,20 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class BLL_Articulo
+    {
+        private DAL_Articulo objCapaDatos = new DAL_Articulo();
+
+        public List<Articulo> ObtenerArticulos()
+        {
+            return objCapaDatos.ObtenerArticulos();
+        }
+    }
+}
