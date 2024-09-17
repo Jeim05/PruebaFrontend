7a0717bae053e57b286f5d74c08e1c5c431c1a46
Jeim05 <lopezvegajeimmy@gmail.com>
2024-09-16 20:53:52 -0600

    Implementación de una nueva tabla y su respectiva funcionalidad para su visualización

--- CapaPresentacion/App_Start/BundleConfig.cs
+++ CapaPresentacion/App_Start/BundleConfig.cs
@@ -8,13 +8,16 @@ namespace CapaPresentacion
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new Bundle("~/bundles/jquery").Include( // SE ELIMINA LA PALABRA SCRIPT ANTES DEL BUNDLE
+                       "~/Scripts/jquery-{version}.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
                         "~/Scripts/menu.js"));
 
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios.  De esta manera estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/site.css"));
